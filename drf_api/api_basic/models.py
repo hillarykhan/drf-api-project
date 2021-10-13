c48b245e9fc0cc658122158757114d7ac79d13cb
Hillary <khan.hillary@gmail.com>
2021-10-12 21:56:33 -0400

    creates Article model

--- drf_api/api_basic/models.py
+++ drf_api/api_basic/models.py
@@ -1,3 +1,16 @@
+from typing import Match
 from django.db import models
 
+from drf_api.drf_api.settings import AUTH_PASSWORD_VALIDATORS
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+        
