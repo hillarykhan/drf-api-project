9f124014f0134b47afabac60f231728bd358e886
Hillary <khan.hillary@gmail.com>
2021-10-20 15:36:43 -0400

    creates generic views

--- drf_api/api_basic/urls.py
+++ drf_api/api_basic/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ArticleAPIView, ArticleDetails
+from .views import ArticleAPIView, ArticleDetails, GenericAPIView
 
 urlpatterns = [
     path('articles/', ArticleAPIView.as_view()),
     path('articles/<int:id>/', ArticleDetails.as_view()),
+    path('generic/articles/<int:id>/', GenericAPIView.as_view()),
 
 
 ]
