e26ffc23451dd6daea0176ac60c8aa6dd5425d57
Hillary <khan.hillary@gmail.com>
2021-10-21 16:34:30 -0400

    creates viewset with routers

--- drf_api/api_basic/urls.py
+++ drf_api/api_basic/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .views import ArticleAPIView, ArticleDetails, GenericAPIView
+from django import urls
+from django.urls import path, include
+from .views import ArticleAPIView, ArticleDetails, GenericAPIView, ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
 
 urlpatterns = [
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:id>/', include(router.urls)),
     path('articles/', ArticleAPIView.as_view()),
     path('articles/<int:id>/', ArticleDetails.as_view()),
     path('generic/articles/<int:id>/', GenericAPIView.as_view()),
